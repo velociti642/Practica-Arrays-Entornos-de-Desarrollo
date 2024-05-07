ce484c1c46ba05fb20e5dcdd5871a45e844930b4
velociti642 <131817459+velociti642@users.noreply.github.com>
2024-05-07 16:45:41 +0200

    Metodo para obtener la nota minima creadaç

---
+++ PracticaArrays/src/model/minimaNota.java
@@ -0,0 +1,21 @@
+package model;
+
+public class minimaNota {
+
+	public float minimaNota(int[] notas) throws IllegalArgumentException {
+		for (int nota : notas) {
+			if (nota < 0 || nota > 10) {
+				throw new IllegalArgumentException("La nota no esta entre 0 y 10");
+	        }
+	    }
+
+		int minNota = notas[0];
+        for (int nota : notas) {
+            if (nota > minNota) {
+                minNota = nota;
+            }
+        }
+
+        return minNota;
+    }
+}
